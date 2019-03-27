3db95febf444d028da7937140839cdc81f0db475
manojpanta <manojpanta95@gmail.com>
2019-03-27 10:27:17 -0600

    setup all the connections in nrel service , using api key stored in application.yml file

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,8 +1,26 @@
 class NrelService
+  def get_stations(params)
+    get_json("api/alt-fuel-stations/v1.json?zip=#{params[:q]}")
+  end
 
-  
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    response = Faraday.new(url: "https://developer.nrel.gov/") do |faraday|
+      faraday.headers['X-API-KEY'] = ENV['NREL_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 
 
+
+
+
+
+  # https://developer.nrel.gov
 
 
 end
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -5,3 +5,17 @@ describe NrelService do
     expect(facade).to be_a(NrelService)
   end
 end
+
+describe 'instance methods' do
+  it '#get_stations returns all the stations' do
+    WebMock.disable!
+    facade = NrelService.new
+    expect(facade.get_stations({q: 80260})).to be_a(Hash)
+  end
+  it '#get_json returns all the all the stations in JSON format' do
+    WebMock.disable!
+    facade = NrelService.new
+    expect(facade.get_json('/api/alt-fuel-stations/v1.json?zip=80260')).to be_a(Hash)
+    expect(facade.get_json('/api/alt-fuel-stations/v1.json?zip=80260')).to have_key(:total_results)
+  end
+end
